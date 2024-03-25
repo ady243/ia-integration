a7c1458a69a4cff89f1367d32b738b32d7ccbc69
Manu1Mackenzii <emmanuelit29@gmail.com>
2024-03-25 11:15:00 +0100

    update de la branche / suppression du MBD template

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
         {/* Logo en haut */}
         <div className="sidebar-logo">
           <img
-            src="../public/images/Food_Store_Logo-removebg-preview.png"
+            src="../images/Food_Store_Logo-removebg-preview.png"
             alt="Logo"
             style={{ width: "250px", height: "200px", marginLeft: "-37px" }}
           />
@@ -39,17 +39,17 @@ const Sidebar = () => {
           </li>
           <li className="sidebar-item" style={{ marginBottom: "50px" }}>
             <a href="#" className="sidebar-link">
-              Messages
+              Recettes
             </a>
           </li>
           <li className="sidebar-item" style={{ marginBottom: "50px" }}>
             <a href="#" className="sidebar-link">
-              Settings
+              Profile
             </a>
           </li>
           <li className="sidebar-item" style={{ marginBottom: "50px" }}>
             <a href="#" className="sidebar-link">
-              Logout
+              Se déconnecter
             </a>
           </li>
         </ul>
