a23dfd7229faf2150b2b12a684cdd27c447763fe
xaless <hsuwang75@gmail.com>
2024-03-27 13:54:33 +0100

    button test

---
+++ client/src/pages/ButtonTestGrocery.jsx
@@ -0,0 +1,41 @@
+import  { useState } from 'react';
+import Button from '../components/Button.jsx';
+import Modal from '../components/Modal.jsx';
+import axios from "axios"
+
+import { API_URL } from '../configUrl';
+
+const App = () => {
+    const [groceryList, setGroceryList] = useState('');
+    const [showModal, setShowModal] = useState(false);
+
+    const generateGroceryList = async () => {
+        try {
+            const response = await fetch(`${API_URL}/api/grocerylist`, {
+                headers:{
+                    'content-type':'application/json'
+                },
+                method:'POST'
+            });
+            const data = await response.json();
+            console.log(data)
+            setGroceryList(data.groceryList);
+            setShowModal(true); // Affiche la modal après avoir reçu les données
+        } catch (error) {
+            console.error('Error fetching grocery list:', error);
+        }
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    return (
+        <div>
+            <Button onClick={generateGroceryList} text="Générer la liste de courses" />
+            <Modal isOpen={showModal} message={groceryList} onClose={handleCloseModal} />
+        </div>
+    );
+};
+
+export default App;
---
+++ client/src/pages/ButtonTestSide.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Button from '../components/Button.jsx';
+import Modal from '../components/Modal.jsx';
+
+import { API_URL } from '../configUrl';
+
+const App = () => {
+    const [sideDish, setSideDish] = useState('');
+    const [showModal, setShowModal] = useState(false);
+
+    const generateSideDish = async () => {
+        try {
+            const response = await fetch(`${API_URL}/api/sidedish`, {
+                headers:{
+                    'content-type':'application/json'
+                },
+                method:'POST'
+            });
+            const data = await response.json();
+            setSideDish(data.response);
+            setShowModal(true);
+        } catch (error) {
+            console.error('Error fetching side dish:', error);
+        }
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    return (
+        <div>
+            <Button onClick={generateSideDish} text="Générer l'accompagnement" />
+            <Modal isOpen={showModal} message={sideDish} onClose={handleCloseModal} />
+        </div>
+
+    );
+};
+
+export default App;
