d94cbb5c3abaaef079a615c7d83ff5da67e5c317
ady243 <masiviady7@gmail.com>
2024-03-27 17:04:07 +0100

    client

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,36 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { HookContext } from '../hook/useHookProvider';
+
 import ChatBot from './ChatBotPage';
-import Button from '../components/Button';
+import Sidebar from '../components/SideBar';
 
 export const HomePage = () => {
-    const { currentUser, logout } = useContext(HookContext); 
-    const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const storedUser = localStorage.getItem('currentUser');
-        if (storedUser) {
-            setUser(JSON.parse(storedUser));
-        }
-    }, []);
-
-    useEffect(() => {
-        if (currentUser) {
-            localStorage.setItem('currentUser', JSON.stringify(currentUser));
-            setUser(currentUser);
-        }
-    }, [currentUser]);
-
-    console.log('currentUser', user);
 
     return (
         <div>
-            <h1>Home Page</h1>
-            <p>Current User: {user ? user.fullName : 'None'}</p> 
+            <Sidebar />
+ 
+           <ChatBot />
 
-            <Button onClick={logout} text="se deconnecter"/>
-
-            <ChatBot />
         </div>
     );
 }
